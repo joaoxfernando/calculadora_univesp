6d4fec3ce194dbb0757ea00b48bdec12de75ae4e
joaoxfernando <joaoxfernando@outlook.com>
2025-10-13 15:14:12 -0300

    Corrigindo arquivo com a GTAG

--- js/gtag.js
+++ js/gtag.js
@@ -1,9 +1,5 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-TKCB1JM6B8"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'G-TKCB1JM6B8');
-</script>
+gtag('config', 'G-TKCB1JM6B8');
